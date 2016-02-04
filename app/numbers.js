634a8021d537e16d85d3cf87af27f25c463f644a
Johnson Quadras <johnson.quadras@sungard.com>
2016-02-04 13:08:45 +0530

    Adding solution for Numbers and Objects.

--- app/numbers.js
+++ app/numbers.js
@@ -6,11 +6,17 @@ exports.numbersAnswers = {
   },
 
   base10: function(str) {
-
+      return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+    var binary = "";
 
+      if(num % 2 != 0) {
+        binary = "0"+(num).toString(2);
+        return binary;
+      }
+     return (num).toString(2);
   },
 
   multiply: function(a, b) {
--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+   return  fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+   constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var properties = [];
+    for(var property in obj) {
+      if(obj.hasOwnProperty(property)) {
+         properties.push(property +":"+obj[property]);
+      }
+    }
 
+    return properties;
   }
 };
